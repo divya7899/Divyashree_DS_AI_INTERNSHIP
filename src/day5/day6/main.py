1ac0d328fd1df4a70735d084303d6e57d1b49412
divyashree <divyashrees988@gmail.com>
2026-02-12 11:38:48 +0530

    Add files via upload

---
+++ src/day5/day6/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  6 14:12:25 2026
+
+@author: User
+"""
+
+import math_operations
+
+result_power = math_operations.power(2, 10)
+result_average = math_operations.average([10, 20, 30, 40])
+
+print("2^10 =", result_power)
+print("Average =", result_average)
---
+++ src/day5/day6/math_operations.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  6 12:57:25 2026
+
+@author: User
+"""
+def power(base, exp):
+    return base ** exp
+
+def average(numbers_list):
+    return sum(numbers_list) / len(numbers_list)
